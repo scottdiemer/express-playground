0d08726ae094fe3ccfbb6437fa5a25506680b4c9
scottdiemer <sdiemer@cfl.rr.com>
2020-11-15 14:47:08 -0500

    added ejs templates

--- app.js
+++ app.js
@@ -8,15 +8,7 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
