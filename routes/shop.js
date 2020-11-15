a7d1935575ec6b038ceaaa089b6fabe130bdfbef
scottdiemer <sdiemer@cfl.rr.com>
2020-11-14 21:45:19 -0500

    setup handlebars templates

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,14 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { pageTitle: "Shop", products: products, path: "/" });
+  res.render("shop", {
+    pageTitle: "Shop",
+    products: products,
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
