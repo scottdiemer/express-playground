6f0db3accf220a5201ffdcef7ba5a3d45fce4f10
scottdiemer <sdiemer@cfl.rr.com>
2020-11-23 20:34:35 -0500

    Loaded product detail data and passing product to cart

--- models/product.js
+++ models/product.js
@@ -37,4 +37,11 @@ module.exports = class Product {
   static fetchAll(callback) {
     getProductsFromFile(callback)
   }
+
+  static findById(id, callback) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id)
+      callback(product)
+    })
+  }
 }
