b46d690fc655e0e389f6cd3e11287e7c42a49955
scottdiemer <sdiemer@cfl.rr.com>
2020-12-15 21:38:22 -0500

    displaying cart items in order then clearing cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -110,9 +110,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart
       return cart.getProducts()
     })
     .then((products) => {
@@ -129,21 +131,23 @@ exports.postOrder = (req, res, next) => {
         .catch((err) => console.log(err))
     })
     .then(() => {
+      fetchedCart.setProducts(null)
+    })
+    .then(() => {
       res.redirect('/orders')
     })
     .catch((err) => console.log(err))
 }
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
-  })
-}
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout',
-  })
+  req.user
+    .getOrders({ include: ['products'] })
+    .then((orders) => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders,
+      })
+    })
+    .catch((err) => console.log(err))
 }
