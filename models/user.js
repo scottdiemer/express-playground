50db9548175587bf62a56f5e454bc81d6182459c
scottdiemer <sdiemer@cfl.rr.com>
2020-12-24 21:08:56 -0500

    adding products to cart in user model

--- models/user.js
+++ models/user.js
@@ -4,14 +4,35 @@ const getDb = require('../util/database').getDb
 const ObjectId = mongodb.ObjectId
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart) {
     this.username = username
     this.email = email
+    this.cart = cart
+    this._id = id
   }
 
   save() {
     const db = getDb()
-    return db.collection('users').insertOne(this)
+    return db
+      .collection('users')
+      .insertOne(this)
+      .then((user) => {
+        console.log(user)
+        return user
+      })
+      .catch((err) => console.log(err))
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id
+    // })
+    const updatedCart = { items: [{ ...product, quantity: 1 }] }
+    const db = getDb()
+    db.collection('users').updateOne(
+      { _id: new ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    )
   }
 
   static findById(userId) {
