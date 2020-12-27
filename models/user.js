b4a768045482ac0f34cdefbb666e358ece6a8ed1
scottdiemer <sdiemer@cfl.rr.com>
2020-12-26 23:14:56 -0500

    setup adding product to cart

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ const getDb = require('../util/database').getDb
 const ObjectId = mongodb.ObjectId
 
 class User {
-  constructor(username, email, cart) {
-    this.username = username
+  constructor(name, email, cart, id) {
+    this.name = name
     this.email = email
     this.cart = cart
     this._id = id
@@ -27,7 +27,9 @@ class User {
     // const cartProduct = this.cart.items.findIndex(cp => {
     //   return cp._id === product._id
     // })
-    const updatedCart = { items: [{ ...product, quantity: 1 }] }
+    const updatedCart = {
+      items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+    }
     const db = getDb()
     db.collection('users').updateOne(
       { _id: new ObjectId(this._id) },
