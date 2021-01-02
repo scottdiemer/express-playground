8bc3027de3037424dc5bcd9bd6fc57582e8daace
scottdiemer <sdiemer@cfl.rr.com>
2021-01-01 21:54:55 -0500

    updated to allow finding and displaying orders

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path')
 
 const shopController = require('../controllers/shop')
 
@@ -19,6 +18,6 @@ router.post('/cart-delete-item', shopController.postCartDeleteProduct)
 
 router.post('/create-order', shopController.postOrder)
 
-// router.get('/orders', shopController.getOrders)
+router.get('/orders', shopController.getOrders)
 
 module.exports = router
